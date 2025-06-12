4dbcae5d81e69847a1b41c31266db000cf6e5201
Sriram Vujjini <sriramvujjini@ip-192-168-1-172.ec2.internal>
2025-06-12 09:22:25 -0500

    checking black formatter

--- checks.py
+++ checks.py
@@ -8,15 +8,18 @@ def check_topmost(topmost, donee_info_gp):
     print(f"Checking topmost result for: {donee_info_gp['name']}")
     return True
 
+
 # preprocesses the strings
 def normalize_address(address):
     # convert country name to standard abb
     country_replacements = {
         r"\b(United States|United States of America | US | U\.S\.A\.?|U\.S\.|America)\b": "USA",
-        r"\b(BHS|Bahamas)": "Bahamas"
-  }
+        r"\b(BHS|Bahamas)": "Bahamas",
+    }
     for pattern in country_replacements:
-        address = re.sub(pattern, country_replacements[pattern], address, flags=re.IGNORECASE)
+        address = re.sub(
+            pattern, country_replacements[pattern], address, flags=re.IGNORECASE
+        )
     address = address.lower()
     return address
 
@@ -28,7 +31,15 @@ def fuzzy_address_check(api_address, GP_address):
 
 
 def autocomplete_check(donee_info_gp):
-    gp_address = donee_info_gp["address"] + ", " + donee_info_gp["city"] + ", " + donee_info_gp["state"] + ", " + donee_info_gp["country"]
+    gp_address = (
+        donee_info_gp["address"]
+        + ", "
+        + donee_info_gp["city"]
+        + ", "
+        + donee_info_gp["state"]
+        + ", "
+        + donee_info_gp["country"]
+    )
     print(f"Autocomplete check for: {donee_info_gp['name']}, address: {gp_address}")
     try:
         autocomplete_results = call_autocomplete(donee_info_gp)
@@ -37,15 +48,24 @@ def autocomplete_check(donee_info_gp):
         return False, ""
     if len(autocomplete_results) > 0:
         for suggestion in autocomplete_results.get("suggestions", []):
-            autocomplete_address = suggestion.get("placePrediction", {}).get("structuredFormat", {}).get("secondaryText", {}).get("text", "")
+            autocomplete_address = (
+                suggestion.get("placePrediction", {})
+                .get("structuredFormat", {})
+                .get("secondaryText", {})
+                .get("text", "")
+            )
             if autocomplete_address:
                 similarity_score = fuzzy_address_check(autocomplete_address, gp_address)
-                print(f"auto address: {autocomplete_address}, donee_info address: {gp_address}, sim_score: {similarity_score}")
+                print(
+                    f"auto address: {autocomplete_address}, donee_info address: {gp_address}, sim_score: {similarity_score}"
+                )
                 if similarity_score > 80:
-                    return True, suggestion.get("placePrediction", {}).get("placeId", "")
+                    return True, suggestion.get("placePrediction", {}).get(
+                        "placeId", ""
+                    )
     return False, ""
 
     # test cases for this function:
-        # check for GPs whose addresses cross 80 threshold
-        # check for GPs whose addresses do not cross 80 but in fact have same addresses
-        # check for GPs who do not have any matching address in autocomplete results
+    # check for GPs whose addresses cross 80 threshold
+    # check for GPs whose addresses do not cross 80 but in fact have same addresses
+    # check for GPs who do not have any matching address in autocomplete results
